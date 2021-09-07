80a6ee9be2f67e4e9d9c05cec605a26e855da9d9
resume-aditya <adityameanstack@gmail.com>
2021-09-07 22:19:47 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,35 @@
+let onPageLoad = () => {
+    addNav();
+    setIFrameURL('bio');
+}
+let addNav = () => {
+    $.ajax('./assets/navData.json')
+    .done(navData => {
+        let ulPlaceholder = document.querySelector('.navLinks ul');
+        navData.forEach((link)=>{
+            let anchor = document.createElement('a'),
+                li = document.createElement('li');
+            anchor.setAttribute('href', '#');
+            anchor.setAttribute('nav', link.page);
+            anchor.text = link.display;
+            li.appendChild(anchor);
+            ulPlaceholder.appendChild(li);
+        });
+        addNavigationListener();
+    });
+}
+let addNavigationListener = () => { //TODO : Read up on arrow fuctions
+    let allNavLinks = document.querySelectorAll('.navLinks ul a'); //TODO : Read up on queryselector
+    allNavLinks.forEach((anchor) => {
+        anchor.addEventListener('click', (e) => {
+            e.preventDefault();
+            setIFrameURL(anchor.getAttribute('nav'));
+        })
+    });
+}
+let setIFrameURL = (page) => {
+    let iframeInstance = document.querySelector('.detail iframe');
+    iframeInstance.setAttribute('src', `./pages/${page}.html`);
+}
+
+onPageLoad();
